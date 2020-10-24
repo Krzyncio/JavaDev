f7664fc751c7d4266f313cf5b293ca38e75321c7
Krz yncio <smolinski.krzysztof@gmail.com>
2020-10-24 12:25:20 +0200

    Feeding dog Add Auto

---
+++ src/com/company/Auto.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Auto {
+    String model;
+    String color;
+    Double capacity;
+    String registrationNumber;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Animal dog = new Animal();
+
+        Animal dog = new Animal();
 	    dog.weight = 20.00;
-    }
-    
+
+        System.out.println(dog.weight);
+
+        Auto car = new Auto();
+        car.capacity = 1900.00;
+        System.out.println(car.capacity);
+}
 }
